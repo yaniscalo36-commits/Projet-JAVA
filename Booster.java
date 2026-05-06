0a9a41b848a4b71bcd55a5e587a931503d38441e
cayanis <yanis.calonge@ynov.com>
2026-05-06 09:21:54 +0200

    ajout fonctionnalités Booster

---
+++ Booster.java
@@ -0,0 +1,14 @@
+public class Booster {
+
+    String nom;
+    int poussee;
+    int masse;
+    int prix;
+
+    public Booster(String nom, int poussee, int masse, int prix) {
+        this.nom = nom;
+        this.poussee = poussee;
+        this.masse = masse;
+        this.prix = prix;
+    }
+}
